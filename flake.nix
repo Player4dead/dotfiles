59a465cc209143fe0716c5688b19553938114f74
Player4dead <player4dead@proton.me>
2026-09-05 18:52:49 +0200

    broken

--- flake.nix
+++ flake.nix
@@ -1,13 +1,3 @@
-# {
-#   description = "A very basic flake";
-
-#   outputs =
-#     let
-#       inputs = import ./.tack;
-#     in
-#     inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
-# }
-
 {
   description = "A very basic flake";
 
@@ -17,12 +7,6 @@
       inputs = (import ./.tack) {
         overrides = args.tackOverrides or { };
       };
-      inherit (inputs.nixpkgs.lib)
-        filesystem
-        hasSuffix
-        ;
     in
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = builtins.filter (hasSuffix ".nix") (filesystem.listFilesRecursive ./modules);
-    };
+    inputs.flake-parts.lib.mkFlake { inherit inputs self; } (inputs.import-tree ./modules);
 }
